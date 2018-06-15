3071a9b33639ac8e3eec750acbe0c5395e2bf5fc
riverlevy <33565504+riverlevy@users.noreply.github.com>
2018-06-15 14:00:09 +0800

    Words_Fliter.py

---
+++ Words_Fliter.py
@@ -0,0 +1,8 @@
+class WordsFliter(object):
+    def getWords():
+        words_bank=[]
+        with open("SensitiveWords.txt") as words_file:
+            words=words_file.readlines()
+            for word in words:
+                words_bank.append(word[0:-1])
+        return words_bank
